919f6d6e714ed5d522f07affc22b2418036573f1
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2017-12-14 17:07:06 -0200

    code from exercise on freecodecamp

--- helloJavascript/index.js
+++ helloJavascript/index.js
@@ -1,4 +1,4 @@
-var linhas = 42;
+/*var linhas = 42;
 var textRow = "";
 var timeNow = Date.now();
 
@@ -21,4 +21,65 @@ for (var i = 0; i < linhas; i++)
 }
 
 var timeResult = timeNow - Date.now();
-console.log("The test take " + timeResult + "seconds");
+console.log("The test take " + timeResult + "seconds");*/
+
+
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop){
+// Only change code below this line
+
+    var index = -1;
+    for (var i = 0; i < contacts.length; i++)
+    {
+        if (contacts[i].firstName == firstName)
+        {
+            index = i;
+        }
+    }
+    if (index == -1)
+    {
+        return "No Such Contact";
+    }
+    if (!contacts[index].hasOwnProperty(prop))
+    {
+        return "No Such Property";
+    }
+    return contacts[index][prop];
+// Only change code above this line
+}
+
+// Change these values to test your function
+console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Harry", "number"));
+console.log(lookUpProfile("Diogo", "likes"));
+console.log(lookUpProfile("Sherlock", "lastName"));
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Kristian", "belongins"));
