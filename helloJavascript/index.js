b460100a2fdfdd26c415944921c87f17554e1d3a
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2017-12-18 17:43:02 -0200

    Some javascript exercises from FreeCodeCamp

--- helloJavascript/index.js
+++ helloJavascript/index.js
@@ -23,7 +23,7 @@ for (var i = 0; i < linhas; i++)
 var timeResult = timeNow - Date.now();
 console.log("The test take " + timeResult + "seconds");*/
 
-
+/*
 //Setup
 var contacts = [
     {
@@ -83,3 +83,66 @@ console.log(lookUpProfile("Diogo", "likes"));
 console.log(lookUpProfile("Sherlock", "lastName"));
 console.log(lookUpProfile("Kristian", "lastName"));
 console.log(lookUpProfile("Kristian", "belongins"));
+*/
+
+/*
+//Setup Exercise Mutation FreeCodeCamp
+function mutation(arr) {
+
+    var primeiraPalavra = arr[0].toUpperCase();
+    var segundaPalavra = arr[1].toUpperCase();
+    var tamanhoSegundaPalavra = segundaPalavra.length;
+    for (var i = 0; i < tamanhoSegundaPalavra; i++) {
+        console.log("Para a letra " + segundaPalavra[i] + " o index é " + primeiraPalavra.indexOf(segundaPalavra[i]));
+        if (primeiraPalavra.indexOf(segundaPalavra[i]) === -1) {
+            return false
+        }
+    }
+    return true;
+  }
+  
+console.log(mutation(["hello", "hey"])); // False
+console.log(mutation(["hello", "Hello"])); // True
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])); //True
+console.log(mutation(["Mary", "Army"])); // True
+console.log(mutation(["Mary", "Aarmy"])); // True
+console.log(mutation(["Alien", "line"])); // True
+console.log(mutation(["floor", "for"])); // True
+console.log(mutation(["hello", "neo"])); // False
+console.log(mutation(["voodoo", "no"])); // False
+
+console.log("Hello".toUpperCase().indexOf("e".toUpperCase()));
+*/
+
+//Setup Exercise Falsy Bouncer FreeCodeCamp
+var arr = [7, false, null, 0, "", undefined, NaN, "ate", 9, "Eita", true];
+
+var resultado = arr.filter(function(e) {
+    return Boolean(e) == true;
+});
+
+console.log(resultado);
+
+// Setup Exercise Seek and Destroy FreeCodeCamp
+/**
+ * You will be provided with an initial array (the first argument in the destroyer function), 
+ * followed by one or more arguments. 
+ * Remove all elements from the initial array that are of the same value as these arguments.
+ * 
+ * Remember to use Read-Search-Ask if you get stuck. Write your own code.
+ * 
+ * Here are some helpful links:
+ * 
+ * Arguments object -> link(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments)
+ * Array.prototype.filter() -> link(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
+ */
+function destroyer(arr) {
+    // Remove all the values
+    return arr;
+}
+  
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3)); // [1,1]
+console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)); // [1,5,1]
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3 ,5)); // [1]
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3)); // []
+console.log(destroyer(["tree", "hambuger", 53], "tree", 53)); // ["hamgurger"]
